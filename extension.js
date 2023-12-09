dcc3abe3d478d8265ed635d8ba4089fdc751545a
Diogo Santos <diogo_goncalves2004@hotmail.com>
2023-12-09 17:57:33 +0000

    Refactor test resolution logic in PrologTestController

--- extension.js
+++ extension.js
@@ -7,6 +7,9 @@ const fs = require('fs');
 // Your extension is activated the very first time the command is executed
 
 class PrologTestController {
+    /**
+     * Represents a constructor for the Prolog Test Controller.
+     */
     constructor() {
         this.controller = vscode.tests.createTestController('prologTestController', 'Prolog Test Controller');
         this.controller.resolveHandler = this.resolveTests.bind(this);
@@ -33,42 +36,107 @@ class PrologTestController {
     }
 
     /**
-     * Resolves the tests by searching for Prolog files in the workspace and creating test items for each test case.
-     * 
-     * @param {vscode.TestRun} run - The TestRun object to enqueue the test items.
-     * @returns {Promise<void>} - A promise that resolves when all the tests have been resolved.
+     * Resolves tests by iterating through all workspace folders and calling resolveTestsInWorkspaceFolder for each folder.
+     * @param {Function} run - The function to run the resolved tests.
+     * @returns {Promise<void>} - A promise that resolves when all tests have been resolved.
      */
     async resolveTests(run) {
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-        for (const workspaceFolder of workspaceFolders) {
-            const prologFiles = await vscode.workspace.findFiles(new vscode.RelativePattern(workspaceFolder, '**/*.pl'), '**/node_modules/**');
-            for (const file of prologFiles) {
-                const content = await fs.promises.readFile(file.fsPath, 'utf8');
-                const lines = content.split('\n');
-                let inTestBlock = false;
-                let currentSuite = null;
-                for (let i = 0; i < lines.length; i++) {
-                    const line = lines[i];
-                    if (line.includes(':- begin_tests(')) {
-                        inTestBlock = true;
-                        const suiteName = line.match(/\(([^)]+)\)/)[1];
-                        const suiteId = `suite:${file.fsPath}:${suiteName}`;
-                        currentSuite = this.controller.createTestItem(suiteId, suiteName, file);
-                        currentSuite.canResolveChildren = true;
-                        this.controller.items.add(currentSuite);
-                    } else if (line.includes(':- end_tests(')) {
-                        inTestBlock = false;
-                        currentSuite = null;
-                    } else if (inTestBlock && line.trim().startsWith('test(')) {
-                        const testName = line.match(/\(([^)]+)\)/)[1];
-                        const test = this.createTest(file, testName, i);
-                        currentSuite.children.add(test);
-                    }
-                }
+        try {
+            const workspaceFolders = vscode.workspace.workspaceFolders;
+            for (const workspaceFolder of workspaceFolders) {
+                await this.resolveTestsInWorkspaceFolder(workspaceFolder);
+            }
+        } catch (err) {
+            console.error(`Failed to resolve tests: ${err}`);
+        }
+    }
+
+    /**
+     * Resolves tests in the specified workspace folder.
+     * 
+     * @param {vscode.WorkspaceFolder} workspaceFolder - The workspace folder to resolve tests in.
+     * @returns {Promise<void>} - A promise that resolves when all tests in the workspace folder have been resolved.
+     */
+    async resolveTestsInWorkspaceFolder(workspaceFolder) {
+        const prologFiles = await vscode.workspace.findFiles(new vscode.RelativePattern(workspaceFolder, '**/*.pl'), '**/node_modules/**');
+        for (const file of prologFiles) {
+            await this.resolveTestsInFile(file);
+        }
+    }
+
+    /**
+     * Resolves tests in a file.
+     * 
+     * @param {Object} file - The file object.
+     * @returns {Promise<void>}
+     */
+    async resolveTestsInFile(file) {
+        let content;
+        try {
+            content = await fs.promises.readFile(file.fsPath, 'utf8');
+        } catch (err) {
+            console.error(`Failed to read file ${file.fsPath}: ${err}`);
+            return;
+        }
+        const lines = content.split('\n');
+        let inTestBlock = false;
+        let currentSuite = null;
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+            if (line.includes(':- begin_tests(')) {
+                currentSuite = this.beginTestSuite(line, file, i);
+                inTestBlock = true;
+            } else if (line.includes(':- end_tests(')) {
+                inTestBlock = false;
+                currentSuite = null;
+            } else if (inTestBlock && line.trim().startsWith('test(')) {
+                this.addTestCase(line, file, i, currentSuite);
             }
         }
     }
 
+    /**
+     * Creates and returns a test suite based on the provided line, file, and lineNumber.
+     * @param {string} line - The line containing the test suite information.
+     * @param {object} file - The file object representing the file containing the test suite.
+     * @param {number} lineNumber - The line number where the test suite is defined.
+     * @returns {object|null} - The created test suite object or null if the suite name extraction fails.
+     */
+    beginTestSuite(line, file, lineNumber) {
+        const match = line.match(/\(([^)]+)\)/);
+        if (!match) {
+            console.error(`Failed to extract suite name from line ${lineNumber} in file ${file.fsPath}`);
+            return null;
+        }
+        const suiteName = match[1];
+        const suiteId = `suite:${file.fsPath}:${suiteName}`;
+        const suite = this.controller.createTestItem(suiteId, suiteName, file);
+        suite.canResolveChildren = true;
+        this.controller.items.add(suite);
+        return suite;
+    }
+
+    /**
+     * Adds a test case to the test suite.
+     * @param {string} line - The line containing the test case.
+     * @param {object} file - The file object.
+     * @param {number} lineNumber - The line number of the test case.
+     * @param {object} suite - The test suite object.
+     */
+    addTestCase(line, file, lineNumber, suite) {
+        const match = line.match(/\(([^)]+)\)/);
+        if (!match) {
+            console.error(`Failed to extract test name from line ${lineNumber} in file ${file.fsPath}`);
+            return;
+        }
+        const testName = match[1];
+        const test = this.createTest(file, testName, lineNumber);
+        suite.children.add(test);
+    }
+
+    /**
+     * Disposes the controller.
+     */
     dispose() {
         this.controller.dispose();
     }
